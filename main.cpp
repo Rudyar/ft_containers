45b6596371eeb3537570d0b58f8a47f8d9d813e1
Arthur <arudy@student.42.fr>
2022-07-05 18:11:49 +0200

    vector reserve & push back done

--- main.cpp
+++ main.cpp
@@ -6,16 +6,38 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:25:13 by arudy             #+#    #+#             */
-/*   Updated: 2022/07/04 13:58:59 by arudy            ###   ########.fr       */
+/*   Updated: 2022/07/05 17:45:10 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include <vector.hpp>
+#include "vector.hpp"
 #include <vector>
+// #include <iostream>
 
 int	main(void)
 {
-	std::vector<int> test;
-	test.size();
+	ft::vector<int> test;
+	test.push_back(34);
+	test.push_back(33);
+	test.push_back(32);
+	test.push_back(31);
+	test.push_back(30);
+	test.push_back(35);
+	test.printVec();
+	std::cout << "Capacity : " << test.capacity() << std::endl;
+	std::cout << "Max size : " << test.max_size() << std::endl;
 	return 0;
 }
+
+// int	main(void)
+// {
+// 	std::vector<int> test;
+// 	test.push_back(34);
+// 	test.push_back(34);
+// 	test.push_back(34);
+// 	test.push_back(34);
+// 	test.push_back(34);
+// 	// test.push_back(34);
+// 	std::cout << test.capacity() << std::endl;
+// 	return 0;
+// }
--- vector.hpp
+++ vector.hpp
@@ -6,11 +6,12 @@
 /*   By: arudy <arudy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 11:31:14 by arudy             #+#    #+#             */
-/*   Updated: 2022/07/05 12:15:30 by arudy            ###   ########.fr       */
+/*   Updated: 2022/07/05 18:11:20 by arudy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <memory>
+#include <exception>
 #include <iostream>
 
 namespace ft
@@ -18,17 +19,12 @@ namespace ft
 	template <class T, class Allocator = std::allocator<T> >
 	class vector
 	{
-		private:
-			size_type	_size;
-			size_type	_capacity;
-			Allocator	_alloc;
-			T			*_vec;
 
 		public:
 			// types:
 			typedef typename Allocator::reference			reference;
 			typedef typename Allocator::const_reference		const_reference;
-			typedef typename Allocator::const_pointer		const_pointer
+			typedef typename Allocator::const_pointer		const_pointer;
 			typedef Allocator								allocator_type;
 			typedef size_t									size_type;
 			typedef int										difference_type;
@@ -39,13 +35,20 @@ namespace ft
 			// typedef implementation defined					iterator;
 			// typedef implementation defined					const_iterator;
 
+		private:
+			size_type		_size;
+			size_type		_capacity;
+			allocator_type	_alloc;
+			T				*_vec;
+
+		public:
 			// Canonical
 			explicit vector(const Allocator& alloc = Allocator())
 			{
 				_size = 0;
 				_capacity = 0;
 				_alloc = alloc;
-				_vec = alloc.allocate(0);
+				_vec = _alloc.allocate(0);
 			}
 
 			explicit vector(size_type n, const T& value = T(), const Allocator& alloc = Allocator())
@@ -60,7 +63,9 @@ namespace ft
 
 			template <class InputIterator>
 			vector(InputIterator first, InputIterator last, const Allocator& = Allocator());
+
 			vector(const vector<T,Allocator>& x);
+
 			~vector()
 			{
 				// _alloc.destroy(_vec, _size);
@@ -83,15 +88,24 @@ namespace ft
 			// const_reverse_iterator rend() const;
 
 			// capacity:
-			size_type size() const;
-			size_type max_size() const;
-			void resize(size_type sz, T c = T());
-			size_type capacity() const;
+			size_type size() const
+			{
+				return _size;
+			}
+
+			size_type max_size() const
+			{
+				return _alloc.max_size();
+			}
+
+			void resize(size_type size, T c = T()); // Appeler reserve !
+
+			size_type capacity() const
 			{
 				return _capacity;
 			}
 
-			bool empty() const;
+			bool empty() const
 			{
 				if (_size != 0)
 					return false;
@@ -100,22 +114,22 @@ namespace ft
 
 			void reserve(size_type n)
 			{
-				T			*tmp;
-
-				if (n + _size > _capacity)
+				if (n > max_size())
+					throw(std::length_error("vector : max size"));
+				if (n > capacity())
 				{
-					_size += n;
-					_capacity += n; // Surement à changer
-					_alloc = Allocator(); // A virer ?
-					tmp = _alloc.allocate(_capacity + n);
-					for (size_t i = 0; i < _capacity + n; i++)
+					T	*tmp;
+					size_type old_capacity = _capacity;
+					_capacity = n;
+					tmp = _alloc.allocate(_capacity);
+					for (size_t i = 0; i < _size; i++)
 					{
-						t_alloc.construct(tmp, _vec[i]);
-						destroy(_vec[i]);
+						_alloc.construct(tmp + i, *_vec + i);
+						_alloc.destroy(&_vec[i]);
 					}
-					deallocate(_vec);
+					_alloc.deallocate(_vec, old_capacity);
+					_vec = tmp;
 				}
-
 			}
 
 			// element access:
@@ -129,7 +143,14 @@ namespace ft
 			// const_reference back() const;
 
 			// modifiers:
-			// void push_back(const T& x);
+			void push_back(const T& x)								// Surement d'autres choses à gérer
+			{
+				reserve(_size + 1);
+				T	*end = &_vec[_size];
+				_size++;
+				_alloc.construct(end, x);
+			}
+
 			// void pop_back();
 			// iterator insert(iterator position, const T& x);
 			// void insert(iterator position, size_type n, const T& x);
@@ -140,6 +161,16 @@ namespace ft
 			// iterator erase(iterator first, iterator last);
 			// void swap(vector<T,Allocator>&);
 			// void clear();
+
+			void	printVec()
+			{
+				std::cout << "-----------------" << std::endl;
+				for (size_t i = 0; i < _size; i++)
+				{
+						std::cout << "Vec i : " << _vec[i] << std::endl;
+				}
+				std::cout << "-----------------" << std::endl;
+			}
 	};
 
 	template <class T, class Allocator>
